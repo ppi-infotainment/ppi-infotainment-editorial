0d7e7b1879998526a6b689238826074b6f888804
beschlz <bendt@schulz-hamburg.de>
2021-10-29 21:31:40 +0200

    #9 implemented manage device settings layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,39 @@ import ManageDevicesPage from './pages/manage_devices/ManageDevicesPage';
 import HomePage from './pages/homepage/Homepage';
 import InfotainmentRoot from './pages/infotainment/Root';
 import EditDeviceContentPage from './pages/edit_device_content/EditDeviceContentPage';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider} from 'baseui';
+
+
 
 function App() {
+  const engine = new Styletron();
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/infotainment/:id">
-          <InfotainmentRoot />
-        </Route>
-        <Route path="/devices/:id">
-          <EditDeviceContentPage />
-        </Route>
-        <Route exact path="/devices">
-          <ManageDevicesPage />
-        </Route>
-        <Route path="/contents">
-          <ManageContentsPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </ Switch >
-    </Router>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Router>
+          <Switch>
+            <Route path="/infotainment/:id">
+              <InfotainmentRoot />
+            </Route>
+            <Route path="/devices/:id/:description">
+              <EditDeviceContentPage />
+            </Route>
+            <Route exact path="/devices">
+              <ManageDevicesPage />
+            </Route>
+            <Route path="/contents">
+              <ManageContentsPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </ Switch >
+        </Router>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
--- src/pages/edit_device_content/EditDeviceContentPage.tsx
+++ src/pages/edit_device_content/EditDeviceContentPage.tsx
@@ -1,15 +1,46 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { useParams } from "react-router-dom";
+import PageHeaderText from "../../components/PageHeaderText/PageHeaderText";
+import AddContentButton from "./components/AddContentButton/AddContentButton";
 import styles from './EditDeviceContentPage.module.css';
+import {
+    List,
+    arrayMove,
+    arrayRemove
+} from "baseui/dnd-list";
+import { Box } from "@mui/system";
 
 const EditDeviceContentPage: FunctionComponent = () => {
-    const { id } = useParams<any>();
+    const { description } = useParams<any>();
+    const [items, setItems] = useState([
+        "Never gonna give you up",
+        "PPI Talks",
+        "BBI"
+    ]);
 
     return (
         <div className={styles.edit_device_content_page}>
-            Hey {id}
+             <Box sx={{ flexDirection: 'column', display: 'flex', alignContent: 'center', alignSelf: 'center' }}>
+            <PageHeaderText title={description + " bearbeiten"} />
+            <Box sx={{maxWidth: "400px"}}>
+                <List
+                items={items}
+                onChange={({ oldIndex, newIndex }) =>
+                    setItems(
+                        newIndex === -1
+                            ? arrayRemove(items, oldIndex)
+                            : arrayMove(items, oldIndex, newIndex)
+                    )
+                }
+            />
+            </Box>
+
+            <AddContentButton />
+            </Box>
         </div>
+
     );
 };
 
 export default EditDeviceContentPage;
+
