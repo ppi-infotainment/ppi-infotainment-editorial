d41978ac3e850c1fb1f026e546e347749c60ae68
beschlz <bendt@schulz-hamburg.de>
2021-10-29 15:11:52 +0200

    setup tils work and started mine

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+import styles from './Homepage.module.css';
+
+const HomePage: FunctionComponent = () => {
+    return (
+        <div className={styles.homepage}>
+            
+        </div>
+    );
+};
+
+
+export default HomePage;
