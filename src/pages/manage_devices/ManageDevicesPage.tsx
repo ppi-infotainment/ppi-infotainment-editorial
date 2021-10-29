71f59812670bdb5de6f4a39d645b436eb7d5f5f5
beschlz <bendt@schulz-hamburg.de>
2021-10-29 20:45:09 +0200

    Added keys for rendering

--- src/pages/manage_devices/ManageDevicesPage.tsx
+++ src/pages/manage_devices/ManageDevicesPage.tsx
@@ -19,8 +19,7 @@ const ManageDevices: FunctionComponent = () => {
         {
             id: "3",
             description: "Düsseldorf Küche"
-        }];
-
+    }];
 
     return (
         <Box className={styles.manage_devices_page}>
@@ -28,7 +27,7 @@ const ManageDevices: FunctionComponent = () => {
                 <PageHeaderText title="Geräte verwalten" />
             </Box>
             <Box sx={{ flexDirection: 'column', display: 'flex', alignContent: 'center', alignSelf: 'center' }}>
-                {mockedDevices.map((device) => <DeviceCard deviceName={device.description} />)}
+                {mockedDevices.map((device) => <DeviceCard deviceName={device.description} key={device.id + "_" + device.description}/>)}
             </Box>
         </Box>
     );
