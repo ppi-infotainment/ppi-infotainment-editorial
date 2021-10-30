67bd3a2ac532920f365aaf9dc3f0f8ae3ee7479b
Til Blechschmidt <til@blechschmidt.de>
2021-10-30 16:01:30 +0200

    :sparkles: Add polling for new content

--- src/components/RemoteInfotainment/RemoteInfotainment.tsx
+++ src/components/RemoteInfotainment/RemoteInfotainment.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import useAxios from 'axios-hooks'
 import InfotainmentContent from "../InfotainmentContent/InfotainmentContent";
 import InfotainmentCarousel from "../InfotainmentCarousel/InfotainmentCarousel";
@@ -10,18 +10,55 @@ export type RemoteInfotainmentProps = {
 
 const RemoteInfotainment: FunctionComponent<RemoteInfotainmentProps> = ({ systemId, duration }) => {
     const endpoint = `https://us-central1-ppi-infotainment.cloudfunctions.net/v1/editorial/${systemId}/full`;
-    const [{ data, loading, error }, refetch] = useAxios(endpoint)
+    const [{ data }, refetch] = useAxios(endpoint);
+    const [content, setContent] = useState([]);
+
+    useEffect(() => {
+        const timeout = setTimeout(refetch, 8000);
+
+        return () => clearTimeout(timeout);
+    }, [data, refetch]);
 
     useEffect(() => {
         const timeout = setTimeout(refetch, 10 * 60 * 1000);
         return () => clearTimeout(timeout);
     }, [refetch]);
 
-    if (loading) return <p>Fetching content...</p>
-    if (error) return <p>Failed to fetch content! {error.toString()}</p>
+    useEffect(() => {
+        if (data !== undefined) {
+            const previousMappedContent: any = {};
+            const mappedContent: any = {};
 
+            data.forEach((value: any) => {
+                mappedContent[value.filename] = { content: value.content, filetype: value.filetype };
+            });
+            
+            content.forEach((value: any) => {
+                previousMappedContent[value.filename] = { content: value.content, filetype: value.filetype };
+            });
+            
+            if (previousMappedContent.length !== mappedContent.length) {
+                setContent(data);
+            } else {
+                const keys = Array.from(new Set(Object.keys(mappedContent).concat(Object.keys(previousMappedContent))));
+                const matches = keys.reduce((prev, key) => {
+                    if (!mappedContent.hasOwnProperty(key) || !previousMappedContent.hasOwnProperty(key)) {
+                        return false;
+                    }
+
+                    const contentMatches = mappedContent[key].content === previousMappedContent[key].content;
+                    const filetypeMatches = mappedContent[key].filetype === previousMappedContent[key].filetype;
+
+                    return prev && contentMatches && filetypeMatches;
+                }, true);
+
+                if (!matches) setContent(data);
+            }
+        }
+    }, [data, content]);
+    
     return <InfotainmentCarousel>
-        {data.map((entry: any, index: number) => {
+        {content.map((entry: any, index: number) => {
             return <InfotainmentContent key={index} filetype={entry.filetype} content={entry.content} duration={duration} onDisplayCompletion={() => {}} />
         })}
     </InfotainmentCarousel>
